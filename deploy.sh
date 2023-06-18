055b878b05665bfa9ae65a0a1bb7c217063e71c3
max529 <maxime.betrisey@hotmail.com>
2023-06-18 15:04:51 +0200

    change deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+git checkout master
+git merge --no-ff develop
+git push
 git checkout -b predev
 git reset --soft `git log origin/master --oneline | tail -1 | awk '{print $1}'`
 git add -A
 git commit -m "deploy on `date +"%Y-%m-%dT%H:%M:%S%:z"`"
 git push origin `git subtree split --prefix dist predev`:deploy --force
-git checkout master
+git checkout develop
 git branch -D predev
